c4cae641480c30968db72280b60846dfeb228c0e
Dalibor Kušić <dalibor.kusic@hotmail.com>
2026-04-23 14:00:24 +0200

    feat: add GitHub Pages deployment

--- mockup/vite.config.ts
+++ mockup/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/sar-operations-planner/',
   // @ts-ignore - vitest injects test config
   test: {
     globals: true,
